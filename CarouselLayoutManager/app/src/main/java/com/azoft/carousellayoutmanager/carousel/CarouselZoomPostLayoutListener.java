51625de98837e4699b185fdbb2e8a3a7b0210464
mig35 <mmig35@gmail.com>
2016-04-01 14:37:23 +0600

    add more comments

--- CarouselLayoutManager/app/src/main/java/com/azoft/carousellayoutmanager/carousel/CarouselZoomPostLayoutListener.java
+++ CarouselLayoutManager/app/src/main/java/com/azoft/carousellayoutmanager/carousel/CarouselZoomPostLayoutListener.java
@@ -3,12 +3,18 @@ package com.azoft.carousellayoutmanager.carousel;
 import android.support.annotation.NonNull;
 import android.view.View;
 
+/**
+ * Implementation of {@link CarouselLayoutManager.PostLayoutListener} that makes interesting scaling of items. <br />
+ * We are trying to make items scaling quicker for closer items for center and slower for when they are far away.<br />
+ * Tis implementation uses atan function for this purpose.
+ */
 public class CarouselZoomPostLayoutListener implements CarouselLayoutManager.PostLayoutListener {
 
     @Override
     public void transformChild(@NonNull final View child, final float itemPositionToCenterDiff, final int orientation) {
         final float scale = (float) (4 * (2 * -StrictMath.atan(Math.abs(itemPositionToCenterDiff) + 1.0) / Math.PI + 1) / 2);
 
+        // because scaling will make view smaller in its center, then we should move this item to the top or bottom to make it visible
         if (CarouselLayoutManager.VERTICAL == orientation) {
             final float translateY = child.getHeight() * (1 - scale) / 2f;
             child.setTranslationY(Math.signum(itemPositionToCenterDiff) * translateY);
